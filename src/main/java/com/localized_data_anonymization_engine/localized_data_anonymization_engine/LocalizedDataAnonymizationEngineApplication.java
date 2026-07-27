590381f70624b8b20780aeb4e44f5186e24f3cc8
Khushi Saini <khushisaini5614@gmail.com>
2026-07-27 21:44:57 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/localized_data_anonymization_engine/localized_data_anonymization_engine/LocalizedDataAnonymizationEngineApplication.java
@@ -0,0 +1,13 @@
+package com.localized_data_anonymization_engine.localized_data_anonymization_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalizedDataAnonymizationEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalizedDataAnonymizationEngineApplication.class, args);
+	}
+
+}
